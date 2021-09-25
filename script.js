7157deaa85e31a882b114c1f6194f549fd914bb6
Getvion <gog0125@mail.com>
2021-09-25 22:38:16 +0300

    доработал отображение цветовых тем, написал функции счета

--- script.js
+++ script.js
@@ -1,18 +1,47 @@
 const body = document.querySelector('body');
 const lightTheme = document.querySelector('.theme__light');
 const darkTheme = document.querySelector('.theme__dark');
-const keys = document.querySelectorAll('.keys');
-const resultField = document.querySelector('.result__field');
-const inputField = document.querySelector('.input__field');
+const sun = document.querySelector('.svg__sun');
+const moon = document.querySelector('.svg__moon');
+
+// изменение цветовых тем
+moon.classList.add('active');
 
 lightTheme.addEventListener('click', () => {
   if (body.classList.contains('dark')) {
     body.classList.remove('dark');
+
+    sun.classList.add('active');
+    moon.classList.remove('active');
   }
 });
 
 darkTheme.addEventListener('click', () => {
   if (!body.classList.contains('dark')) {
     body.classList.add('dark');
+
+    moon.classList.add('active');
+    sun.classList.remove('active');
   }
 });
+
+// функции счета
+function insert(num) {
+  document.form.textview.value += num;
+}
+
+function clean() {
+  document.form.textview.value = '';
+}
+
+function removeOneSimbol() {
+  const exp = document.form.textview.value;
+  document.form.textview.value = exp.substring(0, exp.length - 1);
+}
+
+function equal() {
+  const exp = document.form.textview.value;
+  if (exp) {
+    document.form.textview.value = eval(exp);
+  }
+}
